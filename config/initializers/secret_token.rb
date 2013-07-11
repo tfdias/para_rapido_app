401605b4b45f8f3cdc8a9c03b15381f25d4bf828
Thiago Dias <tfdias@gmail.com>
2013-07-10 22:25:37 -0300

    Commit Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ParaRapidoApp::Application.config.secret_token = 'b1becc90e132cc541842f70568830768a8aacc60e0bf82a2f9ff84f6061f1b2a309c5a243ae781589a19b075152bd009d17b7b8594294e1e4078c4c0da71149e'
